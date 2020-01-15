14b1d666bf4bbe833cfd6d5e624847810a2b83fd
Wallyson <wallyson@aluno.fapce.edu.br>
2020-01-15 15:47:52 -0300

    Finalização da autenticação

--- ufca/conversor/urls.py
+++ ufca/conversor/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('listar-pessoas',pessoa_list,name='pessoas_list'),
     path('registrar_usuario/', registrar_usuario, name='registrar_usuario'),
     path('listar_usuario/', listar_usuario, name='listar_usuario'),
-
+    path('login/', logar_usuario, name='login'),
+    path("remover_usuario/(?P<pk>[0-9]+)/",deletar_usuario, name='remover_usuario'),
+    path('deslogar/',deslogar,name="deslogar"),
 ]
--- ufca/conversor/views.py
+++ ufca/conversor/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 
-
+@login_required
 def registrar_usuario(request, template_name="registrar.html"):
     user = request.user
     if request.method == "POST":
@@ -26,11 +26,45 @@ def registrar_usuario(request, template_name="registrar.html"):
 
         return redirect('/listar_usuario/') 
     return render(request, template_name, {})
+@login_required
 def listar_usuario(request, template_name="listar.html"):
     usuarios = User.objects.all()
     usuario = {'lista': usuarios}
     return render(request, template_name, usuario)
 
+def logar_usuario(request, template_name='login.html'):
+    next = request.GET.get('next','listar_usuario')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(next)
+        else:
+            messages.error(request, 'Usuário ou senha incorretos.')
+            return HttpResponseRedirect(settings.LOGIN_URL)
+    return render(request, template_name, {'redirect_to': next})
+@login_required
+def deletar_usuario(request, pk, template_name='delete.html'):
+    user = request.user
+    if user.has_perm('user.delete_user'):
+        try:
+            usuario = User.objects.get(pk=pk)
+            if request.method == 'POST':
+                usuario.delete()
+                return redirect('listar_usuario')
+        except:
+            messages.error(request,'Usuario não encontrado')
+            return redirect('listar_usuario')
+    else:
+        messages.error(request,'Permissão negada')
+        return redirect('listar_usuario')
+    return render(request,template_name,{'usuario':usuario})
+
+def deslogar(request):
+    logout(request)
+    return HttpResponseRedirect(settings.LOGIN_URL)
 @login_required
 def enviar_arquivo(request):
     template = 'subir_arquivo.html'
